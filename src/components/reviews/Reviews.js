629eff87a187f1ac5da8d4851dff18cf9e749bd4
breannams <breannamarie_s@hotmail.com>
2021-09-20 20:08:20 -0400

    commit tree 62f01e985e8fbb89edd21ad38b8a94ae27742e9c

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+    const {reviews, deleteReview} = this.props
+    const restaurantReviews = reviews.filter(review => review.restaurantId === this.props.restaurantId)
+
+    const reviewList = restaurantReviews.map(review => {
+      return <Review key = {review.id}  review = {review} deleteReview = {deleteReview} />
+    })
     return (
       <ul>
-        Reviews
+        {reviewList}
       </ul>
     );
   }
